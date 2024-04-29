754ce60c77ed7b24e30f62f24018870e574763c1
lzp <1224060318@qq.com>
2024-04-29 23:38:51 +0800

    Feat: 启动页面

--- view/main_window.py
+++ view/main_window.py
@@ -1,5 +1,9 @@
+import os
+
+from PySide6.QtCore import QSize, QEventLoop, QTimer
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
-from qfluentwidgets import FluentIcon as Icon
+from qfluentwidgets import FluentIcon as Icon, SplashScreen
 from qfluentwidgets import (NavigationItemPosition, FluentWindow)
 
 from view.chat_interface import ChatInterface
@@ -8,6 +12,7 @@ from view.home_interface import HomeInterface
 from view.sender_interface import SenderInterface
 from view.settings_interface import SettingsInterface
 from view.timegraph_interface import TimeGraphInterface
+from view.visual_dashboard_interface import VisualDashboardInterface
 
 
 class MainWindow(FluentWindow):
@@ -17,6 +22,7 @@ class MainWindow(FluentWindow):
 
     def __init__(self):
         super().__init__()
+        self._init_window()
 
         # create sub interface
         self.home_interface = HomeInterface(self)
@@ -24,10 +30,15 @@ class MainWindow(FluentWindow):
         self.graph_interface = GraphInterface(self)
         self.timegraph_interface = TimeGraphInterface(self)
         self.settings_interface = SettingsInterface(self)
+        self.visual_dashboard_interface = VisualDashboardInterface(self)
         self.sender_interface = SenderInterface(self)
 
         self._init_navigation()
-        self._init_window()
+        # 延时启动页面
+        loop = QEventLoop(self)
+        QTimer.singleShot(2000, loop.quit)
+        loop.exec()
+        self.splashScreen.finish()
 
     def _init_navigation(self):
         """
@@ -38,6 +49,7 @@ class MainWindow(FluentWindow):
         self.addSubInterface(self.sender_interface, Icon.SEND, '定时发送')
         self.addSubInterface(self.graph_interface, Icon.TILES, '图表')
         self.addSubInterface(self.timegraph_interface, Icon.MARKET, '时间图')
+        self.addSubInterface(self.visual_dashboard_interface, Icon.MOVIE, "仪表盘")
 
         self.navigationInterface.addSeparator()
 
@@ -52,7 +64,17 @@ class MainWindow(FluentWindow):
         self.setMinimumWidth(760)
         self.setWindowTitle('ChatSecretary')
 
+        basedir = os.path.join(os.path.dirname(__file__), '..')
+        self.icon = QIcon(os.path.join(basedir, 'resource', 'images', 'icon.png'))
+        self.icon.setIsMask(False)
+        self.setWindowIcon(self.icon)
+        self.splashScreen = SplashScreen(self.icon, self, False)
+        self.splashScreen.setIconSize(QSize(250, 250))
+        self.splashScreen.raise_()
+
         # position in center
         desktop = QApplication.screens()[0].availableGeometry()
         w, h = desktop.width(), desktop.height()
         self.move(w // 2 - self.width() // 2, h // 2 - self.height() // 2)
+        self.show()
+        QApplication.processEvents()
